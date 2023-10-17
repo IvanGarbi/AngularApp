afd352acacaa07dd7a4130e88ed74596e51b2166
IvanGarbi <ivan_garbi@hotmail.com>
2023-10-17 17:40:57 -0300

    tratamento de erro 404

--- AngularApp/src/app/app.routes.ts
+++ AngularApp/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from "./navegacao/home/home.component";
 import { ContatoComponent } from "./institucional/contato/contato.component";
 import { SobreComponent } from "./institucional/sobre/sobre.component";
 import { CadastroComponent } from "./demos/reactiveForms/cadastro/cadastro.component";
+import { NotFoundComponent } from "./navegacao/not-found/not-found.component";
 
 export const rootRouterConfig: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -12,7 +13,8 @@ export const rootRouterConfig: Routes = [
     { path: 'contato', component: ContatoComponent },
     { path: 'sobre', component: SobreComponent },
     { path: 'cadastro', component: CadastroComponent },
-    { path: 'produtos', loadChildren: () => import('./demos/arquitetura-componentes/produto.module').then(m => m.ProdutoModule) } // lazy loading!
+    { path: 'produtos', loadChildren: () => import('./demos/arquitetura-componentes/produto.module').then(m => m.ProdutoModule) }, // lazy loading!
+    { path: '**', component: NotFoundComponent } // sempre por último
 ];
 
 @NgModule({
