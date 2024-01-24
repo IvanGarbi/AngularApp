bf4e4038407dfd55b34f2cdc04a92ac099c09e42
Ivan Assumpcao <Ivan.Assumpcao@landisgyr.com>
2024-01-24 15:06:51 -0300

    Route Guards - CanLoad, CanActivate

--- AngularApp/src/app/app.routes.ts
+++ AngularApp/src/app/app.routes.ts
@@ -6,14 +6,16 @@ import { ContatoComponent } from "./institucional/contato/contato.component";
 import { SobreComponent } from "./institucional/sobre/sobre.component";
 import { CadastroComponent } from "./demos/reactiveForms/cadastro/cadastro.component";
 import { NotFoundComponent } from "./navegacao/not-found/not-found.component";
+import { AuthGuard } from "./services/app.guard";
 
 export const rootRouterConfig: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'contato', component: ContatoComponent },
-    { path: 'sobre', component: SobreComponent },
+    { path: 'sobre', component: SobreComponent }, // canActivate: [AuthGuard] verifica se a rota pode ser usada.
     { path: 'cadastro', component: CadastroComponent },
-    { path: 'produtos', loadChildren: () => import('./demos/arquitetura-componentes/produto.module').then(m => m.ProdutoModule) }, // lazy loading!
+    { path: 'produtos', loadChildren: () => import('./demos/arquitetura-componentes/produto.module').then(m => m.ProdutoModule),
+        canLoad: [AuthGuard], canActivate: [AuthGuard] }, // lazy loading!
     { path: '**', component: NotFoundComponent } // sempre por último
 ];
 
